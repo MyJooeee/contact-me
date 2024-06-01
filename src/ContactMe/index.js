f7ae5bf3ac33a2c99371cc693d4338ca07a507d8
MyJooeee <dancette.jonathan@gmail.com>
2024-06-01 11:28:17 +0200

    Layout

--- src/ContactMe/index.js
+++ src/ContactMe/index.js
@@ -1,7 +1,7 @@
 // Core
 import * as React from 'react';
 // Components
-import { blueGrey } from '@mui/material/colors';
+import { blueGrey, grey } from '@mui/material/colors';
 import { AppBar, Avatar, Button, Container, Divider, Stack, Toolbar, Typography, useScrollTrigger } from '@mui/material';
 import Accordion from './Accordion';
 // import { useTheme } from '@mui/material/styles';
@@ -9,7 +9,7 @@ import Jonathan from '../Images/jonathan.jpg';
 // import ScrollToTop from './ScrollToTop';
 
 // ---------------------------------------------------------------------------------
-const navItems = ['Home', 'Experiences', 'Diplomas', 'Skills'];
+const navItems = ['Home', 'Experiences', 'Curriculum', 'Skills', 'Hobbies'];
 
 const ContactMe = () => {
 
@@ -18,7 +18,7 @@ const ContactMe = () => {
   // JSX -------------------------------------------------------------------------
   return (
     <Container  id="back-to-top-anchor" maxWidth={false} disableGutters>
-      <Stack sx={{ alignItems: 'center', backgroundColor: blueGrey[700], gap: 1, py: 1 }}>
+      <Stack sx={{ alignItems: 'center', backgroundColor: blueGrey[900], gap: 1, py: 1 }}>
         <Avatar alt="Jonathan Dancette" src={Jonathan} sx={{ width: 96, height: 96 }} />
         <Typography variant="h4" sx={{ color: 'white' }}>
           Jonathan Dancette
@@ -31,49 +31,58 @@ const ContactMe = () => {
         </Typography>
       </Stack>
       <AppBar position="sticky">
-        <Toolbar>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-        >
-          JD
-        </Typography>
-        <Stack direction="row">
+        <Toolbar disableGutters sx={{ columnGap: 5, pl: 5, flexWrap: 'wrap' }}>
           {navItems.map((item) => (
-            <Button key={item} sx={{ color: '#fff' }}>
+            <Button key={item} sx={{ color: 'white' }}>
               {item}
             </Button>
           ))}
-        </Stack>
         </Toolbar>
       </AppBar>
-      <Toolbar />
-      <Stack sx={{ gap: 1 }}>
-        <Typography variant="h4">
-          Experiences
-        </Typography>
-        <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
-        <Stack>
-          <Accordion />
+      <Stack 
+        sx={{ 
+          border: '1px solid red', 
+          backgroundColor: grey[200],
+          gap: 5,
+          py: 2, 
+          pl: 5, 
+          pr: 15
+        }}>
+        <Stack sx={{ border: '1px solid blue', gap: 2 }}>
+          <Typography variant="h4">
+            Experiences
+          </Typography>
+          <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
+          <Stack sx={{ pl:2 }}>
+            <Accordion />
+          </Stack>
         </Stack>
-      </Stack>
-      <Stack sx={{ gap: 1 }}>
-        <Typography variant="h4">
-          Experiences
-        </Typography>
-        <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
-        <Stack>
-          <Accordion />
+        <Stack sx={{ gap: 1 }}>
+          <Typography variant="h4">
+          Curriculum
+          </Typography>
+          <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
+          <Stack sx={{ pl: 2 }}>
+            <Accordion />
+          </Stack>
         </Stack>
-      </Stack>
-      <Stack sx={{ gap: 1 }}>
-        <Typography variant="h4">
-          Experiences
-        </Typography>
-        <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
-        <Stack>
-          <Accordion />
+        <Stack sx={{ gap: 1 }}>
+          <Typography variant="h4">
+            Skills
+          </Typography>
+          <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
+          <Stack sx={{ pl: 2 }}>
+            <Accordion />
+          </Stack>
+        </Stack>
+        <Stack sx={{ gap: 1 }}>
+          <Typography variant="h4">
+            Hobbies
+          </Typography>
+          <Divider sx={{ bgcolor: (theme) => theme.palette.info.light }}/>
+          <Stack sx={{ pl: 2 }}>
+            <Accordion />
+          </Stack>
         </Stack>
       </Stack>
     </Container>
