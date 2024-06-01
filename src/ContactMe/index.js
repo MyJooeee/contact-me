60800c7722167972f98402ab21845a10e3c85466
MyJooeee <dancette.jonathan@gmail.com>
2024-06-01 13:29:29 +0200

    Background image

--- src/ContactMe/index.js
+++ src/ContactMe/index.js
@@ -6,6 +6,7 @@ import { AppBar, Avatar, Button, Container, Divider, Stack, Toolbar, Typography,
 import Accordion from './Accordion';
 // import { useTheme } from '@mui/material/styles';
 import Jonathan from '../Images/jonathan.jpg';
+import Earth from '../Images/earth.jpg';
 import Contact from './Contact';
 // import ScrollToTop from './ScrollToTop';
 
@@ -19,7 +20,7 @@ const ContactMe = () => {
   // JSX -------------------------------------------------------------------------
   return (
     <Container  id="back-to-top-anchor" maxWidth={false} disableGutters>
-      <Stack sx={{ alignItems: 'center', backgroundColor: blueGrey[900], gap: 1, p: 1 }}>
+      <Stack sx={{ alignItems: 'center', backgroundImage: `url(${Earth})`, gap: 1, p: 1 }}>
         <Avatar alt="Jonathan Dancette" src={Jonathan} sx={{ width: 96, height: 96 }} />
         <Typography variant="h4" sx={{ color: 'white' }}>
           Jonathan Dancette
@@ -86,12 +87,28 @@ const ContactMe = () => {
         </Stack>
       </Stack>
       <Stack>
-        <Stack direction="row" sx={{ backgroundColor: blue[800], height: 30, justifyContent: 'center', gap: 5, py: 1 }}>
+        <Stack 
+          direction="row" 
+          sx={{ 
+            backgroundColor: blue[800], 
+            height: 30, 
+            justifyContent: 'center', 
+            gap: 5, 
+            pt: 1 
+          }}
+        >
           <Typography variant="body2" sx={{ color: 'white' }} >Linkedin</Typography>
           <Typography variant="body2" sx={{ color: 'white' }}>Github</Typography>
           <Typography variant="body2" sx={{ color: 'white' }}>HyperSciences</Typography>
         </Stack>
-        <Stack direction="row" sx={{ backgroundColor: blue[900], justifyContent: 'flex-start', py: 1, pl: 5 }}>
+        <Stack 
+          direction="row" 
+          sx={{ 
+            backgroundColor: blue[900], 
+            justifyContent: 'flex-start', 
+            py: 1, 
+            pl: 5 
+          }}>
           <Typography variant="body2" sx={{ color: 'white' }} >© 2024 Jonathan Dancette | v1.0.0 </Typography>
         </Stack>
       </Stack>
