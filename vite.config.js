0b7cc6086236f2d069939a4289f1437adbe351cc
MyJooeee <dancette.jonathan@gmail.com>
2026-03-27 12:27:04 +0100

    Add base vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    base: '/contact-me/',
     outDir: 'build',
     rolldownOptions: {
     output: {
